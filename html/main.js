cd47672acd5eb65432b7b4a3f37bd265dcdd210f
Jennie20173 <cle14202@gmail.com>
2023-05-19 13:35:49 +0700

    flower

---
+++ html/main.js
@@ -0,0 +1,9 @@
+// function bigmouse() {
+//    document.getElementById('smallmouse').innerHTML= 'xinchao'
+// }
+//  let a = prompt('nhập giá trị a');
+//  let b = prompt(' nhập giá trị  b');
+//  if (a % b == 0) 
+// alert('a là bội sô của b');
+//  else
+/* alert('a không là bội số của b')*/
